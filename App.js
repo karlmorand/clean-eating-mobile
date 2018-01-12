d4eb5a5222abc658893a9208b68a97fea3720c0d
Karl Morand <karljmorand@gmail.com>
2018-01-11 21:34:13 -0500

    Basic Auth0 config working

--- App.js
+++ App.js
@@ -5,53 +5,59 @@
  */
 
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+import Auth0 from 'react-native-auth0';
+
+const auth0 = new Auth0({ domain: 'clean-eating.auth0.com', clientId: 'F4tg24oB2Xm6VsQ5eUhISkykK4S5T8xy' });
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+	ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+	android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu'
 });
 
 export default class App extends Component<{}> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
-    );
-  }
+	constructor(props) {
+		super(props);
+		this.state = { accessToken: null };
+	}
+	showLogin = () => {
+		auth0.webAuth
+			.authorize({ scope: 'openid profile email', audience: 'https://clean-eating.auth0.com/userinfo' })
+			.then(
+				credentials => console.log(credentials)
+				// Successfully authenticated
+				// Store the accessToken
+			)
+			.catch(error => console.log(error));
+	};
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.welcome}>Welcome to React Native!</Text>
+				<Text style={styles.instructions}>To get started, edit App.js</Text>
+				<Text style={styles.instructions}>{instructions}</Text>
+				<Button onPress={this.showLogin} title="Login" />
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF'
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5
+	}
 });
