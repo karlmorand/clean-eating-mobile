cb96f545bcf26e342472474e497b9e4dd8e76730
Karl Morand <karljmorand@gmail.com>
2018-01-25 16:29:23 -0500

    Update onboarding with level descriptions

--- src/screens/Onboarding.js
+++ src/screens/Onboarding.js
@@ -17,14 +17,32 @@ class Onboarding extends Component {
 	render() {
 		return (
 			<View style={containerStyle}>
-				<Text>Choose your challenge level & tap Submit</Text>
+				<Text style={styles.levelTitle}>Choose your challenge level & tap Submit</Text>
 				<ButtonGroup
-					selectedBackgroundColor="pink"
+					selectedBackgroundColor="#30a350"
+					textStyle={{ fontWeight: 'bold' }}
 					onPress={this.handleChange}
 					selectedIndex={this.state.challengeLevel}
 					buttons={['Lifestyle', 'Jump Start', 'Performance']}
-					containerStyle={{ height: 30 }}
+					containerStyle={{ height: 40, marginBottom: 30 }}
 				/>
+
+				<Text style={styles.levelTitle}>Lifestyle</Text>
+				<Text style={styles.textBody}>
+					For those who would like to eat for health and nutrition. Body Composition isn't the focus but rather learning
+					and completing long term lifestyle changes that will be the "baseline" we live most of our lives on.
+				</Text>
+				<Text style={styles.levelTitle}>Jump Start</Text>
+				<Text style={styles.textBody}>
+					For those who don't know what "macros" are. Diet soda is a breakfast food and the idea of measuring food is
+					overwhelming. We all start somewhere. Let's start here.
+				</Text>
+				<Text style={styles.levelTitle}>Performance</Text>
+				<Text style={styles.textBody}>
+					Body composition is a must due to your sport. Being lean and strong is what will take your fitness to the next
+					level. You understand the benefits of weighing and measuring your food. It's just awful to not have anyone to
+					complain to or share recipes with.
+				</Text>
 				<Button onPress={this.handleSubmit} title="Submit" />
 			</View>
 		);
@@ -36,6 +54,10 @@ const styles = StyleSheet.create({
 		textAlign: 'center',
 		fontSize: 20,
 		fontWeight: 'bold'
+	},
+	textBody: {
+		margin: 10,
+		fontSize: 15
 	}
 });
 
