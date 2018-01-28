dda550a9fa7476a900554618b5bb90227e18d151
Karl Morand <karljmorand@gmail.com>
2018-01-27 21:33:00 -0500

    Update settings screen style

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -15,14 +15,22 @@ export default class SettingsScreen extends React.Component {
 
 		// <Button title="Go back to home tab" onPress={() => goBack()} />
 		return (
-			<View style={containerStyle}>
-				<Text>Settings Screen</Text>
+			<View style={styles.container}>
+				<Text style={styles.title}>Settings</Text>
 				<Button onPress={this.logout} title="Logout" />
 			</View>
 		);
 	}
 }
 const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF',
+		paddingLeft: 5,
+		paddingRight: 5
+	},
 	title: {
 		textAlign: 'center',
 		fontSize: 30
