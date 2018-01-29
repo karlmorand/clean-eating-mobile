0543998f1875b18db7191b145b4f19271c3df714
Karl Morand <karljmorand@gmail.com>
2018-01-29 00:13:42 -0500

    UI improvement to login screen

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Image } from 'react-native';
 
 class Login extends Component {
 	handlePress = () => {
@@ -8,10 +8,10 @@ class Login extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
+				<Text style={styles.title}>Broccoli</Text>
+				<Image style={styles.logo} source={require('../images/broccoli_logo.png')} />
 				<Text style={styles.title}>Clean Eating Challenge</Text>
-				<Text style={styles.textBody}>
-					Welcome to the Clean Eating Challenge, tap the button below to login or create an account...
-				</Text>
+				<Text style={styles.textBody}>Welcome to the challenge, tap the button below to login...</Text>
 				<Button onPress={this.handlePress} title="Get Started..." />
 			</View>
 		);
@@ -36,6 +36,11 @@ const styles = StyleSheet.create({
 		margin: 10,
 		fontSize: 17,
 		textAlign: 'center'
+	},
+	logo: {
+		width: 80,
+		height: 80,
+		margin: 10
 	}
 });
 
