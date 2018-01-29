ac8068e7c9b77a9ac79e44ebc80c6317aa454373
Karl Morand <karljmorand@gmail.com>
2018-01-28 21:55:21 -0500

    Show user picture on settings screen

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { navigate } from 'react-navigation';
 import { Button, View, StyleSheet, Text, Image } from 'react-native';
+import { Avatar } from 'react-native-elements';
 import { containerStyle } from '../config';
 
 export default class SettingsScreen extends React.Component {
@@ -17,6 +18,10 @@ export default class SettingsScreen extends React.Component {
 		return (
 			<View style={styles.container}>
 				<Text style={styles.title}>Settings</Text>
+				{this.props.screenProps.user && (
+					<Avatar large rounded source={{ uri: this.props.screenProps.user.picture }} activeOpacity={0.7} />
+				)}
+				<Text style={styles.title}>{this.props.screenProps.user && this.props.screenProps.user.name}</Text>
 				<Button onPress={this.logout} title="Logout" />
 			</View>
 		);
