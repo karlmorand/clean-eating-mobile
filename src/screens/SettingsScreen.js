b45adf97f192872629822e0a03f2c692e4d2470a
Karl Morand <karljmorand@gmail.com>
2018-02-08 11:58:08 -0500

    Add food quote and adjust layout

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -16,12 +16,14 @@ export default class SettingsScreen extends React.Component {
 
 		// <Button title="Go back to home tab" onPress={() => goBack()} />
 		return (
-			<View style={styles.container}>
-				<Text style={styles.title}>Settings</Text>
+			<View style={[containerStyle, styles.container]}>
 				{this.props.screenProps.user && (
 					<Avatar large rounded source={{ uri: this.props.screenProps.user.picture }} activeOpacity={0.7} />
 				)}
 				<Text style={styles.title}>{this.props.screenProps.user && this.props.screenProps.user.name}</Text>
+				<Text style={[styles.textBody, styles.quote]}>
+					"Eat meat and vegetables, nuts and seeds, some fruit, little starch, and no sugar."
+				</Text>
 				<Button onPress={this.logout} title="Logout" />
 			</View>
 		);
@@ -29,8 +31,7 @@ export default class SettingsScreen extends React.Component {
 }
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		justifyContent: 'center',
+		// justifyContent: 'center',
 		alignItems: 'center',
 		backgroundColor: '#F5FCFF',
 		paddingLeft: 5,
@@ -39,5 +40,18 @@ const styles = StyleSheet.create({
 	title: {
 		textAlign: 'center',
 		fontSize: 30
+	},
+	levelTitle: {
+		textAlign: 'center',
+		fontSize: 25,
+		fontWeight: 'bold'
+	},
+	textBody: {
+		margin: 15,
+		fontSize: 17,
+		lineHeight: 22
+	},
+	quote: {
+		fontStyle: 'italic'
 	}
 });
